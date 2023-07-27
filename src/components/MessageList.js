d2cf57c329b1544c41f1db1aaffdd3e49884a72d
susi <susi.franco32@gmail.com>
2023-07-27 17:42:39 -0400

    Created MessageList, and Message component, as well as menu to toggle selection from both.

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,2 +1,43 @@
-import react from react;
-asdfasd
+import React from "react";
+import PropTypes from 'prop-types';
+import Message from './Message';
+import './MessageList.css';
+
+
+
+const MessageList = (props) =>{
+    return (
+        <div class="messages">
+            {
+                props.messages.map((message) => (
+                    < Message 
+                        key={ message.id } 
+                        userId={ message.userId }
+                        title= { message.title }
+                        text = { message.text }
+                        // audio = { message.audio }
+                        recipientId = { message.recipientId }
+                        isSent = {message.isSent}
+                    />
+                ))
+            }
+        </div>
+    );
+};
+
+MessageList.propTypes = {
+    messages: PropTypes.arrayOf (
+        PropTypes.shape({
+            userId: PropTypes.number.isRequired,
+            title: PropTypes.string.isRequired,
+            text: PropTypes.string.isRequired,
+            // audio: PropTypes.string.isRequired,
+            recipientId: PropTypes.number.isRequired,
+            isSent: PropTypes.bool.isRequired
+        })
+        //add create new message function required
+        //load messages function(api call)
+    )
+}
+
+export default MessageList
