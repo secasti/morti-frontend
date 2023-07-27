cd96c950f29920b77c9e7c44ca8b818d084032a7
selene castillo <secast@live.com>
2023-07-27 11:31:47 -0700

    Establish components

--- src/components/Trustee.js
+++ src/components/Trustee.js
@@ -1,1 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './Trustee.css';
+
+const Trustee = (props) => {
+
+    return (
+        <section>
+            This the Trustee component
+        </section>
+    );
+};
+
+Trustee.propTypes = {
+    trustee_id: PropTypes.number,
+    trustee_name: PropTypes.string.isRequired,
+    trustee_email: PropTypes.string.isRequired
+}
+
+export default Trustee;
